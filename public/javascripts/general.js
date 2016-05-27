0e173931163890df1a9d76c9df76d101a69e2ed7
camilosampedro <camilo.sampedro@udea.edu.co>
2016-05-27 11:27:31 -0500

    Added modal fix

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $("a.mobile").click(function () {
         $(".sidebar").slideToggle('fast');
     });
+    $(".commandboxbutton").click(function (event) {
+        console.log("Clicked " + event.target.id)
+        $("#commandbox .modal-body #shutdown").attr("href","/computer/shutdown/" + event.target.id)
+        $("#commandbox .modal-body #upgrade").attr("href","/computer/upgrade/" + event.target.id)
+        $("#commandbox .modal-body #unfreeze").attr("href","/computer/unfreeze/" + event.target.id)
+        $('#commandbox').modal('show');
+    })
 });
