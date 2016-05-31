c2315385977fb7ebd07d879b8cf0f0452fadc4b2
camilosampedro <camilo.sampedro@udea.edu.co>
2016-05-31 13:34:21 -0500

    Scheduled checker time checker added

--- app/config/ComputerChecker.scala
+++ app/config/ComputerChecker.scala
@@ -3,35 +3,49 @@ package config
 import javax.inject._
 
 import akka.actor._
-import dao.{ComputerDAO, ComputerStateDAO, ConnectedUserDAO}
+import dao.{ComputerStateDAO, ConnectedUserDAO}
 import model.{ComputerState, ConnectedUser}
 import services.{ComputerService, SSHOrderService}
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
-class ComputerChecker @Inject()(connectedUserDAO: ConnectedUserDAO, computerStateDAO: ComputerStateDAO, computerService: ComputerService, sSHOrderService: SSHOrderService, actorSystem: ActorSystem) extends UntypedActor {
+class ComputerChecker @Inject()(connectedUserDAO: ConnectedUserDAO, computerStateDAO: ComputerStateDAO, computerService: ComputerService, sSHOrderService: SSHOrderService, actorSystem: ActorSystem, executionContext: ExecutionContext) extends UntypedActor {
   @scala.throws[Exception](classOf[Exception])
-  override def onReceive(message: Any): Unit =  {
+  override def onReceive(message: Any): Unit = {
+
+
     play.Logger.debug("Executing computer checker.")
-    val task = computerService.listAllSimple.map{ computers =>
-      play.Logger.debug(s"""$computers""")
-      computers.map{ computer =>
-        play.Logger.debug("Checking: " + computer)
-        sSHOrderService.check(computer)("Scheduled Checker")
+    time {
+      val task = computerService.listAllSimple.map { computers =>
+        play.Logger.debug(s"""$computers""")
+        computers.map { computer =>
+          play.Logger.debug("Checking: " + computer)
+          sSHOrderService.check(computer)("Scheduled Checker")
+        }
+      }
+      val results: Seq[(ComputerState, Seq[ConnectedUser])] = Await.result(task, Duration.Inf)
+      play.Logger.debug(s"""Computers checked, proceeding to save: $results""")
+      for (result <- results) {
+        val computerState = result._1
+        val addComputerStateTask = computerStateDAO.add(computerState)
+        Await.result(addComputerStateTask, Duration.Inf)
+        val connectedUsers = result._2
+        val addConnectedUsersTasks = connectedUsers.map {
+          connectedUserDAO.add
+        }
+        val f = Future.sequence(addConnectedUsersTasks.toList)
+        Await.result(f, Duration.Inf)
       }
     }
-    val results: Seq[(ComputerState, Seq[ConnectedUser])] = Await.result(task,Duration.Inf)
-    play.Logger.debug(s"""Computers checked, proceeding to save: $results""")
-    for (result <- results) {
-      val computerState = result._1
-      val addComputerStateTask = computerStateDAO.add(computerState)
-      Await.result(addComputerStateTask,Duration.Inf)
-      val connectedUsers = result._2
-      val addConnectedUsersTasks = connectedUsers.map{connectedUserDAO.add}
-      val f = Future.sequence(addConnectedUsersTasks.toList)
-      Await.result(f,Duration.Inf)
-    }
+  }
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    play.Logger.debug("Elapsed time: " + (t1 - t0) + "ns")
+    result
   }
 }
