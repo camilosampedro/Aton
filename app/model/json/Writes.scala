b228b92b2cd592fa0aaaba0cf2ea18025595eae8
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2016-06-10 12:47:33 -0500

    Fixing state API request

--- app/model/json/Writes.scala
+++ app/model/json/Writes.scala
@@ -8,38 +8,38 @@ import play.api.libs.json.{Json, Writes}
   */
 object Writes {
   implicit val computerWrites = new Writes[Computer] {
-    def writes(computer: Computer) = Json.obj {
-      "name" -> computer.name
-      "description" -> computer.description
-      "roomID" -> computer.roomID
+    def writes(computer: Computer) = Json.obj (
+      "name" -> computer.name,
+      "description" -> computer.description,
+      "roomID" -> computer.roomID,
       "ip" -> computer.ip
-    }
+    )
   }
 
   implicit val laboratoryWrites = new Writes[Laboratory] {
-    def writes(laboratory: Laboratory) = Json.obj {
-      "id" -> laboratory.id
-      "name" -> laboratory.name
-      "location" -> laboratory.location
+    def writes(laboratory: Laboratory) = Json.obj (
+      "id" -> laboratory.id,
+      "name" -> laboratory.name,
+      "location" -> laboratory.location,
       "administration" -> laboratory.administration
-    }
+    )
   }
 
   implicit val roomWrites = new Writes[Room] {
-    def writes(room: Room) = Json.obj {
-      "id" -> room.id
-      "name" -> room.name
-      "audiovisualResources" -> room.audiovisualResources
-      "basicTools" -> room.basicTools
+    def writes(room: Room) = Json.obj (
+      "id" -> room.id,
+      "name" -> room.name,
+      "audiovisualResources" -> room.audiovisualResources,
+      "basicTools" -> room.basicTools,
       "laboratoryId" -> room.laboratoryID
-    }
+    )
   }
 
   implicit val connectedUserWrites = new Writes[ConnectedUser] {
-    def writes(connectedUser: ConnectedUser) = Json.obj {
-      "id" -> connectedUser.id
+    def writes(connectedUser: ConnectedUser) = Json.obj (
+      "id" -> connectedUser.id,
       "username" -> connectedUser.username
-    }
+    )
   }
 
   implicit val computerStateWrites = new Writes[ComputerState] {
@@ -54,10 +54,10 @@ object Writes {
     def writes(state: Option[(ComputerState, Seq[ConnectedUser])]) = {
       state match {
         case Some(pair) => {
-          Json.obj {
-            "state" -> Json.toJson(pair._1)
+          Json.obj (
+            "state" -> Json.toJson(pair._1),
             "users" -> pair._2.map(Json.toJson(_))
-          }
+          )
         }
         case _ => Json.parse("")
       }
@@ -72,10 +72,10 @@ object Writes {
   }
 
   implicit val roomsWrites = new Writes[(Room, Seq[(Computer, Option[(ComputerState, Seq[ConnectedUser])])])] {
-    def writes(room: (Room, Seq[(Computer, Option[(ComputerState, Seq[ConnectedUser])])])) = Json.obj {
-      "room" -> Json.toJson(room._1)
+    def writes(room: (Room, Seq[(Computer, Option[(ComputerState, Seq[ConnectedUser])])])) = Json.obj (
+      "room" -> Json.toJson(room._1),
       "computers" -> room._2.map(Json.toJson(_))
-    }
+    )
   }
 
   implicit val listOfRoomsWrites = new Writes[Seq[(Room, Seq[(Computer, Option[(ComputerState, Seq[ConnectedUser])])])]] {
@@ -85,10 +85,10 @@ object Writes {
   }
 
   implicit val laboratoryWithRoomsWrites = new Writes[(model.Laboratory, Seq[(model.Room, Seq[(model.Computer, Option[(model.ComputerState, Seq[model.ConnectedUser])])])])] {
-    def writes(laboratoryWithRooms: (model.Laboratory, Seq[(model.Room, Seq[(model.Computer, Option[(model.ComputerState, Seq[model.ConnectedUser])])])])) = Json.obj {
-      "laboratory" -> Json.toJson(laboratoryWithRooms._1)
+    def writes(laboratoryWithRooms: (model.Laboratory, Seq[(model.Room, Seq[(model.Computer, Option[(model.ComputerState, Seq[model.ConnectedUser])])])])) = Json.obj (
+      "laboratory" -> Json.toJson(laboratoryWithRooms._1),
       "rooms" -> Json.toJson(laboratoryWithRooms._2)
-    }
+    )
   }
 
 
