421e4dc770791c90c15965838854933197e0b6c7
upescatore <ubaldopescatore@gmail.com>
2016-10-11 19:16:27 +0200

    added ScalaTest 3.0.0 support

---
+++ test/controllers/admin/AtonTestSpec.scala
@@ -0,0 +1,12 @@
+package controllers.admin
+
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+
+class AtonTestSpec extends FlatSpec with Matchers {
+  
+  "Aton test spec" should "run properly" in {
+    assert(true)
+  }
+  
+}
